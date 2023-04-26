3827cdb0ce30ae2e943cfd96f66b0f3ce4b35780
Pietromuresu <muresupietro66@gmail.com>
2023-04-26 18:19:34 +0200

    Create and import AppHero in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 
 <script>
     import AppTitle from "./components/AppTitle.vue"
+    import AppHero from "./components/AppHero.vue"
     export default {
       name: "App",
       components: {
-            AppTitle
+            AppTitle,
+            AppHero
           },
       data(){
         return{
@@ -16,9 +18,9 @@
     }
 </script>
 
-
 <template>
-  <AppTitle/>
+<!-- Ora inserisco solo AppHero perchè l'ho montato in AppHero-->
+  <AppHero />
 
 </template>
 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,32 @@
+<script>
+import AppTitle from "./AppTitle.vue"
+export default {
+  name: "AppHero",
+  data(){
+    return{
+
+    }
+  },
+
+  components:{
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  <div>
+    <AppTitle/>
+  </div>
+</template>
+
+<style scoped>
+div{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 500px;
+  background-image: url("../assets/Img/jumbotron-bg.webp");
+}
+</style>
